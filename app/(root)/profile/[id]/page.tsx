436ed2f9c514a4baacf2c846ce08a4d4c5ab9333
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2023-12-26 10:29:03 +0530

    profile header

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,33 @@
+import PostThread from "@/components/forms/PostThread";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const page = async ({params}: {params: {id: string}}) => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(params.id);
+
+  if (!userInfo?.onboarded) {
+    redirect('/onboarding');
+    return null; // Ensure you return null if the user is not onboarded
+  }
+
+  return (
+    <section>
+      <ProfileHeader
+accountId = {userInfo.id}
+authUserId= {user.id}
+name={userInfo.name}
+username={userInfo.username}
+imgUrl={userInfo.image}
+bio={userInfo.bio}
+      />
+    </section>
+  )
+}
+
+export default page
