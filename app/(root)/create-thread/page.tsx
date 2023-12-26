fe488235f41f21fe6d50c86f607b1e735ce513ab
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2023-12-26 22:29:33 +0530

    DEPLOYMENT

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,27 +1,22 @@
-import PostThread from "@/components/forms/PostThread";
-import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-export async function Page() {
-  const user = await currentUser();
+import PostThread from "@/components/forms/PostThread";
+import { fetchUser } from "@/lib/actions/user.actions";
 
+async function Page() {
+  const user = await currentUser();
   if (!user) return null;
 
+  // fetch organization list created by user
   const userInfo = await fetchUser(user.id);
-
-  if (!userInfo?.onboarded) {
-    redirect('/onboarding');
-    return null; // Ensure you return null if the user is not onboarded
-  }
-
-  // Convert userInfo._id to a simple value (e.g., string or number)
-  const userId = userInfo._id.toString(); // Assuming _id is an ObjectId and converting it to a string
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
     <>
-      <h1 className="head-text">Create thread</h1>
-      <PostThread userId={userId} />
+      <h1 className='head-text'>Create Thread</h1>
+
+      <PostThread userId={userInfo._id} />
     </>
   );
 }
